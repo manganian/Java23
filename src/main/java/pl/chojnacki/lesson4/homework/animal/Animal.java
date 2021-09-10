bbc76b3b814d0c0db4d7bcf89a34b3f31c203eeb
Dawid Chojnacki <manganian1990@gmail.com>
2021-09-11 01:32:10 +0200

    Added 2 classes - Animal and Cat

---
+++ src/main/java/pl/chojnacki/lesson4/homework/animal/Animal.java
@@ -0,0 +1,9 @@
+package pl.chojnacki.lesson4.homework.animal;
+
+public class Animal {
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/pl/chojnacki/lesson4/homework/animal/Cat.java
@@ -0,0 +1,7 @@
+package pl.chojnacki.lesson4.homework.animal;
+
+public class Cat extends Animal {
+    public Cat(String name) {
+        super(name);
+    }
+}
