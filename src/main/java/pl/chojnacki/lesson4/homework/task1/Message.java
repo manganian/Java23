39f0213402ff61d528b3e7b48598f01c40b91efe
Dawid Chojnacki <manganian1990@gmail.com>
2021-09-11 00:17:14 +0200

    Added 3 classes Message and Main class

---
+++ src/main/java/pl/chojnacki/lesson4/homework/task1/Message.java
@@ -0,0 +1,5 @@
+package pl.chojnacki.lesson4.homework.task1;
+
+public abstract class Message {
+    public abstract String getMessage();
+}
---
+++ src/main/java/pl/chojnacki/lesson4/homework/task1/WindowMessage.java
@@ -0,0 +1,9 @@
+package pl.chojnacki.lesson4.homework.task1;
+
+public class WindowMessage extends Message {
+
+    @Override
+    public String getMessage() {
+        return "This is class Window Message.";
+    }
+}
