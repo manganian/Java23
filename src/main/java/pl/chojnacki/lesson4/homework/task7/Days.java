bca852bf114c4afd45cb2f844060e857ee8848c3
Dawid Chojnacki <manganian1990@gmail.com>
2021-09-11 06:05:10 +0200

    Added enum and method.

---
+++ src/main/java/pl/chojnacki/lesson4/homework/task7/Days.java
@@ -0,0 +1,11 @@
+package pl.chojnacki.lesson4.homework.task7;
+
+public enum Days {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+}
---
+++ src/main/java/pl/chojnacki/lesson4/homework/task7/NameOfTheWeek.java
@@ -0,0 +1,8 @@
+package pl.chojnacki.lesson4.homework.task7;
+
+public class NameOfTheWeek {
+
+    public static String printNameOfTheWeek (Days day) {
+        return String.valueOf(day);
+    }
+}
